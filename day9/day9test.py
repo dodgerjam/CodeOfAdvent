d3730060bd1ad0636d9ce5595ad673dd8dc5566d
Jamie <dodgerbarker@gmail.com>
2021-01-18 19:02:37 +0000

    Updated day9 testing

--- day9/day9test.py
+++ day9/day9test.py
@@ -10,10 +10,22 @@ class TestSum(unittest.TestCase):
         self.assertEqual(day9pt1(x, prev=5), 127, "Should be 127")
     
     def testpt2(self):
+        text_file = open("day9/input.txt", "r")
+        x = text_file.readlines()
+        text_file.close()
+        self.assertEqual(day9pt1(x), 1309761972, "Should be 1309761972")
+    
+    def testpt3(self):
         text_file = open("day9/input_test.txt", "r")
         x = text_file.readlines()
         text_file.close()
         self.assertEqual(day9pt2(x, prev=5), 62, "Should be 62")
+    
+    def testpt4(self):
+        text_file = open("day9/input.txt", "r")
+        x = text_file.readlines()
+        text_file.close()
+        self.assertEqual(day9pt2(x), 177989832, "Should be 177989832")
     
    
 if __name__ == "__main__":
