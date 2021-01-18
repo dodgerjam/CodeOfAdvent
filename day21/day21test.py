8a815b80d3c18332dda215dd3ae8bf262a65d932
Jamie <dodgerbarker@gmail.com>
2021-01-18 21:01:33 +0000

    Updated tests for day21

--- day21/day21test.py
+++ day21/day21test.py
@@ -7,13 +7,25 @@ class TestSum(unittest.TestCase):
         text_file = open("day21/input_test.txt", "r")
         x = text_file.readlines()
         text_file.close()
-        self.assertEqual(day21pt1(x), 5)
+        self.assertEqual(day21pt1(x), 5, "Should be 5")
 
     def testpt2(self):
+        text_file = open("day21/input.txt", "r")
+        x = text_file.readlines()
+        text_file.close()
+        self.assertEqual(day21pt1(x), 2176, "Should be 2176")
+
+    def testpt3(self):
         text_file = open("day21/input_test.txt", "r")
         x = text_file.readlines()
         text_file.close()
-        self.assertEqual(day21pt2(x), "mxmxvkd,sqjhc,fvjkl")
+        self.assertEqual(day21pt2(x), "mxmxvkd,sqjhc,fvjkl", "Should be mxmxvkd,sqjhc,fvjkl")
+
+    def testpt4(self):
+        text_file = open("day21/input.txt", "r")
+        x = text_file.readlines()
+        text_file.close()
+        self.assertEqual(day21pt2(x), "lvv,xblchx,tr,gzvsg,jlsqx,fnntr,pmz,csqc", "Should be lvv,xblchx,tr,gzvsg,jlsqx,fnntr,pmz,csqc")
    
 if __name__ == "__main__":
     unittest.main()
