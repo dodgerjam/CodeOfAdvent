64cb81a030c216f3627749f5803e56daa2b5ac6b
Jamie <dodgerbarker@gmail.com>
2021-01-18 18:38:39 +0000

    updated day6 testing

--- day6/day6test.py
+++ day6/day6test.py
@@ -9,9 +9,19 @@ class TestSum(unittest.TestCase):
         self.assertEqual(day6pt1(data), 11, "Should equal 11")
 
     def testpt2(self):
+        with open("day6/input.txt", 'r') as file:
+                data = file.read()
+        self.assertEqual(day6pt1(data), 6775, "Should equal 6775")
+
+    def testpt3(self):
         with open("day6/input_test.txt", 'r') as file:
                 data = file.read()
         self.assertEqual(day6pt2(data), 6, "Should equal 6")
+
+    def testpt4(self):
+        with open("day6/input.txt", 'r') as file:
+                data = file.read()
+        self.assertEqual(day6pt2(data), 3356, "Should equal 3356")
    
 if __name__ == "__main__":
     unittest.main()
