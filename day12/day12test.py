b1cda9989855a3348f8e328bbdd068dd9957ff19
Jamie <dodgerbarker@gmail.com>
2021-01-18 19:33:23 +0000

    Updated day12 testing

--- day12/day12test.py
+++ day12/day12test.py
@@ -7,13 +7,25 @@ class TestSum(unittest.TestCase):
         text_file = open("day12/input_test.txt", "r")
         x = text_file.readlines()
         text_file.close()
-        self.assertEqual(day12pt1(x), 25)
-    
+        self.assertEqual(day12pt1(x), 25, "Should be 25")
+
     def testpt2(self):
+        text_file = open("day12/input.txt", "r")
+        x = text_file.readlines()
+        text_file.close()
+        self.assertEqual(day12pt1(x), 1007, "Should be 1007")
+    
+    def testpt3(self):
         text_file = open("day12/input_test.txt", "r")
         x = text_file.readlines()
         text_file.close()
-        self.assertEqual(day12pt2(x), 286)
+        self.assertEqual(day12pt2(x), 286, "Should be 286")
+
+    def testpt4(self):
+        text_file = open("day12/input.txt", "r")
+        x = text_file.readlines()
+        text_file.close()
+        self.assertEqual(day12pt2(x), 41212, "Should be 41212")
     
    
 if __name__ == "__main__":
