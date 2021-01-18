127261ffe4354d775c1f235f195fa86830f69714
Jamie <dodgerbarker@gmail.com>
2021-01-18 19:08:23 +0000

    Updated day11 tests

--- day10/day10test.py
+++ day10/day10test.py
@@ -7,25 +7,25 @@ class TestSum(unittest.TestCase):
         text_file = open("day10/input_test.txt", "r")
         x = text_file.readlines()
         text_file.close()
-        self.assertEqual(day10pt1(x), 220)
+        self.assertEqual(day10pt1(x), 220, "Should be 220")
 
     def testpt2(self):
         text_file = open("day10/input.txt", "r")
         x = text_file.readlines()
         text_file.close()
-        self.assertEqual(day10pt1(x), 2484)
+        self.assertEqual(day10pt1(x), 2484, "Should be 2484")
     
     def testpt3(self):
         text_file = open("day10/input_test.txt", "r")
         x = text_file.readlines()
         text_file.close()
-        self.assertEqual(day10pt2(x), 19208)
+        self.assertEqual(day10pt2(x), 19208, "Should be 19208")
 
     def testpt4(self):
         text_file = open("day10/input.txt", "r")
         x = text_file.readlines()
         text_file.close()
-        self.assertEqual(day10pt2(x), 15790581481472)
+        self.assertEqual(day10pt2(x), 15790581481472, "Should be 15790581481472")
     
    
 if __name__ == "__main__":
--- day11/day11test.py
+++ day11/day11test.py
@@ -7,13 +7,26 @@ class TestSum(unittest.TestCase):
         text_file = open("day11/input_test.txt", "r")
         x = text_file.readlines()
         text_file.close()
-        self.assertEqual(day11pt1(x), 37)
-    
+        self.assertEqual(day11pt1(x), 37, "Should be 37")
+
     def testpt2(self):
+        text_file = open("day11/input.txt", "r")
+        x = text_file.readlines()
+        text_file.close()
+        self.assertEqual(day11pt1(x), 2321, "Should be 2321")
+    
+    
+    def testpt3(self):
         text_file = open("day11/input_test.txt", "r")
         x = text_file.readlines()
         text_file.close()
-        self.assertEqual(day11pt2(x), 26)
+        self.assertEqual(day11pt2(x), 26, "Should be 37")
+    
+    def testpt4(self):
+        text_file = open("day11/input.txt", "r")
+        x = text_file.readlines()
+        text_file.close()
+        self.assertEqual(day11pt2(x), 2102, "Should be 2102")
     
    
 if __name__ == "__main__":
