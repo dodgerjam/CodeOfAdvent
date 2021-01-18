72f3ffe83673b4e55d35227d3771a696d30f5c5c
Jamie <dodgerbarker@gmail.com>
2021-01-18 21:20:19 +0000

    Updated tests for day25

--- day25/day25test.py
+++ day25/day25test.py
@@ -7,7 +7,13 @@ class TestSum(unittest.TestCase):
         text_file = open("day25/input_test.txt", "r")
         x = text_file.readlines()
         text_file.close()
-        self.assertEqual(day25pt1(x), 14897079)
+        self.assertEqual(day25pt1(x), 14897079, "Should be 14897079")
+
+    def testpt1(self):
+        text_file = open("day25/input.txt", "r")
+        x = text_file.readlines()
+        text_file.close()
+        self.assertEqual(day25pt1(x), 18293391, "Should be 18293391")
 
 if __name__ == "__main__":
     unittest.main()
